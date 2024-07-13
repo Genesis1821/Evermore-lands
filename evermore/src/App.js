5f6d5931d99d3f371bc10d9005bca95538a75ca2
Genesis1821 <80156265+Genesis1821@users.noreply.github.com>
2024-07-13 09:42:15 -0400

    ultimos cambios css

--- evermore/src/App.js
+++ evermore/src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import RegistroPropiedades from './paginas/registro_propiedades';
+import LandingPage from './paginas/home';
 
 
 
@@ -7,7 +7,7 @@ function App() {
   return (
     <div className="App">
    
-        <RegistroPropiedades />
+        <LandingPage />
     </div>
   );
 }
