85d69fbcd5d9111c46939f2d7e80857ea3b62588
Genesis1821 <80156265+Genesis1821@users.noreply.github.com>
2024-07-20 20:26:15 -0400

    Funcionalidad del navbar

--- evermore/src/componentes/Navbar/navbar.js
+++ evermore/src/componentes/Navbar/navbar.js
@@ -1,15 +1,18 @@
+import { Link} from 'react-router-dom';
 import './navbar.css'
 
 function Navbar(){
+    
     return(
         <>
         <nav>
             <h2>Everhome</h2>
             <ul className="listPages">
-                <li><a className="navEnlace" href="../paginas/home.js">Landing Page</a></li>
-                <li><a className="navEnlace" href="../paginas/propiedades.js">Propiedades</a></li>
-                <li><a className="navEnlace" href="../paginas/registro_propiedades.js">Registro de propiedades</a></li>
-                <li><a className="navEnlace" href="../paginas/sing_up.js">Inicion de sesion</a></li>
+                <Link to={"/"} className='navEnlace'>Landing Page</Link>
+                <Link to={"/propiedades"} className='navEnlace'>Propiedades</Link>
+                <Link to={"/registro_propiedades"} className='navEnlace'>Registro de propiedades</Link>
+                <Link to={"/registro_personas"} className='navEnlace'>Inicion de sesion</Link>
+
             </ul>
         </nav>
         </>
