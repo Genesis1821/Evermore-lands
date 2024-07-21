f23c29e3d3e3ec3a0d54066215e1cbfd83731f0e
Genesis1821 <80156265+Genesis1821@users.noreply.github.com>
2024-07-21 12:52:34 -0400

    arreglos comits anteriores

--- evermore/src/App.js
+++ evermore/src/App.js
@@ -1,6 +1,6 @@
 import { Routes, Route } from "react-router-dom"
 import './App.css';
-import Home from './paginas/home'
+import LandingPage from './paginas/home'
 import Propiedades from './paginas/propiedades'
 import RegistroPropiedades from './paginas/registro_propiedades'
 import InicioSesion from './paginas/sing_up'
@@ -9,7 +9,7 @@ function App() {
   return (
     <div className="App">   
      <Routes>
-      <Route path="/" element={ <Home />} />
+      <Route path="/" element={ <LandingPage />} />
       <Route path="/propiedades" element={ <Propiedades />} />
       <Route path="/registroPropiedades" element={ <RegistroPropiedades />} />
       <Route path="/registroPersonas" element={ <InicioSesion />} />
